0f5aded980e46acb6f70112bb43ea67d57efdff3
Anand Suthar <anand880441@gmail.com>
2025-12-09 18:42:44 +0530

    Add Problem Solving Level 2

---
+++ JavaScript/Problem_solving/lvl2/all.js
@@ -0,0 +1,113 @@
+//11
+
+// var num = 50
+// var str = " "
+
+// for(var i = 1; i<=num; i++){
+//     str = str + i + " "
+// }
+// console.log(str)
+
+
+//12
+
+// var num = 50
+// // var str =" "
+
+// for(var i = 1; i<=num; i++){
+//     if(i %2 == 0){
+//         str = str + i + " "
+//     }
+// }
+// console.log(str)
+
+
+//13
+
+// var num = 10
+// var str = " "
+
+// for(var i = 1; i<=num; i++){
+//     if(i %2 != 0){
+//         str = str + i + " "
+//     }
+// }
+// console.log(str)
+
+
+//14
+
+// var num = 10
+// var sum = 0
+
+// for(var i = 1; i<=num; i++){
+//     sum = sum + i
+// }
+// console.log(sum)
+
+//15
+
+// var num = 4
+// var product = 1
+
+// for(var i = 1; i<=num; i++){
+//     product = product * i
+// }
+// console.log(product)
+
+
+//16
+
+// function table(num){
+//     var str = ""; 
+
+//     for(var i = 1; i <= 10; i++){
+//         var pro = num * i;
+//         str += pro + " "; 
+//     }
+//     console.log(str);
+// }
+
+// table(5)
+
+
+//17
+
+// function divisible(num){
+// var count = 0
+
+//     for(var i = 1; i<=num; i++){
+//         if(i %3 == 0){
+//             count++
+//         }
+//     }
+//     console.log(count)
+// }
+// divisible(10)
+
+
+//19
+
+// function factr(num){
+// var fact = 1
+
+// for (var i = 1; i <= num; i++){
+//     fact = fact*i
+// }
+// console.log(fact)
+// }
+
+// factr(5)
+
+//19
+
+// function divisible(num){
+// var str = " "
+
+//     for(var i = 1; i<=num; i++){
+//         if(i %7 == 0){
+//             str = str + i + " "
+//         }
+//     }console.log(str)
+// }
+// divisible(70)
