9fd3254e2334286f717b1b4acd2abe69524fa955
Anand Suthar <anand880441@gmail.com>
2025-12-10 13:55:29 +0530

    Add new Methods

--- JavaScript/Problem_solving/lvl6/Q54Minimum in Array.js
+++ JavaScript/Problem_solving/lvl6/Q54Minimum in Array.js
@@ -1,9 +1,22 @@
-var arr = [-1, -2, -3];
-var min = arr[0];
+// var arr = [-1, -2, -3];
+// var min = arr[0];
 
-for (let i = 0; i < arr.length; i++){
-    if (arr[i] < min) {
-        min = arr[i];
-   }
-}
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] < min) {
+//         min = arr[i];
+//    }
+// }
+// console.log(min)
+
+
+// var arr = [1, 2, 3, 9, 80];
+// var min = Math.min(...arr);
+// console.log(min);
+
+
+
+var arr = [1, 2, 3, 4, 0, -8]
+
+let sortedArr = arr.sort((a, b) => b - a)
+let min = sortedArr[sortedArr.length - 1]
 console.log(min)
