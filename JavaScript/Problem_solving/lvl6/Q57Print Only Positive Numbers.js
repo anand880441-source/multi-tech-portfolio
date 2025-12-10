e36e989a73b5120e0bc593c88453d913cfdb1a53
Anand Suthar <anand880441@gmail.com>
2025-12-10 14:15:32 +0530

    Add new Methods

--- JavaScript/Problem_solving/lvl6/Q57Print Only Positive Numbers.js
+++ JavaScript/Problem_solving/lvl6/Q57Print Only Positive Numbers.js
@@ -1,9 +1,16 @@
-var arr = [1, 2, 3, -4];
+// var arr = [1, 2, 3, -4];
 
 
-for (let i = 0; i < arr.length; i++){
-    if (arr[i] > 0) {
-        console.log(arr[i])
-    }
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] > 0) {
+//         console.log(arr[i])
+//     }
+// }
+
+
+
+function countEvenWithFilter(arr) {
+    return arr.filter(num => num>0).length;
 }
 
+console.log(countEvenWithFilter([2,4,6,9,8,1,-5]))
