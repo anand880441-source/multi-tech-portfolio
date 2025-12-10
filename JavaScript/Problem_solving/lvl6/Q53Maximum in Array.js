bcec33e28d224ef4b9216e88d5d2b85f3402265b
Anand Suthar <anand880441@gmail.com>
2025-12-10 13:55:03 +0530

    Add new Methods

--- JavaScript/Problem_solving/lvl6/Q53Maximum in Array.js
+++ JavaScript/Problem_solving/lvl6/Q53Maximum in Array.js
@@ -1,9 +1,24 @@
-var arr = [-1, -2, -3];
-var max = arr[0];
+// var arr = [-1, -2, -3];
+// var max = arr[0];
 
-for (let i = 0; i < arr.length; i++){
-    if (arr[i] > max) {
-        max = arr[i];
-   }
-}
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] > max) {
+//         max = arr[i];
+//    }
+// }
+// console.log(max)
+
+
+
+
+// var arr = [1, 2, 3, 9, 80];
+// var max = Math.max(...arr);
+// console.log(max);
+
+
+
+var arr = [1,2,3,4,5,8]
+
+let sortedArr = arr.sort((a, b) => a - b)
+let max = sortedArr[sortedArr.length - 1]
 console.log(max)
