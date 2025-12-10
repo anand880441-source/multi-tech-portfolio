2310f4788f600b7fd9a5d19b9871a7439b2bdbff
Anand Suthar <anand880441@gmail.com>
2025-12-10 19:38:38 +0530

    Add new Methods

--- JavaScript/Problem_solving/lvl7/Q62Square Each Element.js
+++ JavaScript/Problem_solving/lvl7/Q62Square Each Element.js
@@ -1,9 +1,10 @@
-function squareElement(arr) {
-    
-    for (var i = 0; i < arr.length; i++){
-        var arr2 = arr[i]*arr[i];
-        console.log(arr2)
+function squElement(arr) {
+
+    var arr2 = [];
+    for (var i = 0; i < arr.length; i++) {
+        arr2.push(arr[i] * arr[i]);
     }
+    console.log(arr2);
 }
 
-squareElement([1, 2, 3, 4, 5, 6])
+doubleElement([1, 2, 3, 4, 5, 6]);
