b5bd41bb9f36247c4dff6384d75f4ec956ab53e6
Anand Suthar <anand880441@gmail.com>
2025-12-10 19:39:07 +0530

    Add new Methods

--- JavaScript/Problem_solving/lvl7/Q63Reverse Array into New Array.js
+++ JavaScript/Problem_solving/lvl7/Q63Reverse Array into New Array.js
@@ -9,3 +9,22 @@ function reverseArray(arr) {
 }
 
 console.log(reverseArray([1, 2, 3, 4]));
+
+
+
+// var arr = [1, 2, 3, 4, 5, 6, 7]
+//let rev = new Array(...arr);
+// console.log(rev.reverse())
+
+
+let arr = [1,2,3,4,5,6]
+
+let i =0;
+let j = arr.length-1;
+
+while(i<=j){
+    [arr[i],arr[j] = arr[j],arr[i]];
+    i++;
+    j--;
+}
+console.log(arr)
