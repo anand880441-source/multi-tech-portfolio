bdea84031b6157a5fade2cf351e74354edd18dc3
Anand Suthar <anand880441@gmail.com>
2025-12-10 14:22:51 +0530

    Add new Methods

--- JavaScript/Problem_solving/lvl6/Q59Print Elements Greater than 10.js
+++ JavaScript/Problem_solving/lvl6/Q59Print Elements Greater than 10.js
@@ -1,9 +1,17 @@
-var arr = [1, 2, 3, -40, 12, 15];
+// var arr = [1, 2, 3, -40, 12, 15];
 
 
-for (let i = 0; i < arr.length; i++){
-    if (arr[i] > 10 ) {
-        console.log(arr[i])
-    }
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] > 10 ) {
+//         console.log(arr[i])
+//     }
+// }
+
+
+
+function printGreaterThan10Filter(arr) {
+    arr.filter(num => num > 10)
+        .forEach(num => console.log(num));
 }
 
+printGreaterThan10Filter([1,2,2,4,4,5,47,4,1])
