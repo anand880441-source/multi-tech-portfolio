2fbee5712a3fc49587860f5869fb669596f462de
Anand Suthar <anand880441@gmail.com>
2025-12-10 14:12:16 +0530

    Add new Methods

--- JavaScript/Problem_solving/lvl6/Q56 Count Odd Numbers in Array.js
+++ JavaScript/Problem_solving/lvl6/Q56 Count Odd Numbers in Array.js
@@ -1,9 +1,18 @@
-var arr = [1, 2, 3, 4];
-var count = 0;
+// var arr = [1, 2, 3, 4];
+// var count = 0;
 
-for (let i = 0; i < arr.length; i++){
-    if (arr[i] % 2 != 0) {
-        count++;
-    }
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] % 2 != 0) {
+//         count++;
+//     }
+// }
+// console.log(count)
+
+
+
+
+function countEvenWithFilter(arr) {
+    return arr.filter(num => num % 2 === 1).length;
 }
-console.log(count)
+
+console.log(countEvenWithFilter([2,4,6,9,8,1,5]))
