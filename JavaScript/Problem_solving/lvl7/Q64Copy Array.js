e11589aad6fb841a61cb45b1af0b40c18fcd7146
Anand Suthar <anand880441@gmail.com>
2025-12-12 13:25:33 +0530

    Add new Methods

--- JavaScript/Problem_solving/lvl7/Q64Copy Array.js
+++ JavaScript/Problem_solving/lvl7/Q64Copy Array.js
@@ -11,10 +11,30 @@
 // console.log(reverseArray([1, 2, 3, 4]));
 
 
-function copyArr(arr) {
-    let copy = [...arr];
-    console.log(arr);
-    console.log(copy)
-}
 
-copyArr([1,2,3,4,5,6])
+
+// function copyArr(arr) {
+//     let copy = [...arr];
+//     console.log(arr);
+//     console.log(copy)
+// }
+
+// copyArr([1,2,3,4,5,6])
+
+
+
+
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// var arr1 = arr.map(data => {
+//     return data
+// })
+
+// console.log(arr1)
+
+
+
+var arr = [1, 2, 3, 4, 5]
+
+let copyArr = new Array(...arr)
+console.log(copyArr)
