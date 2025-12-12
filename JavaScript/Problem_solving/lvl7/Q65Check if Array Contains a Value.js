cb470695de4de68dcd1efdeab52ae6fa31f7e6d1
Anand Suthar <anand880441@gmail.com>
2025-12-12 13:25:44 +0530

    Add new Methods

--- JavaScript/Problem_solving/lvl7/Q65Check if Array Contains a Value.js
+++ JavaScript/Problem_solving/lvl7/Q65Check if Array Contains a Value.js
@@ -1,11 +1,14 @@
-function findValue(arr) {
-    let value = 2;
+// function findValue(arr) {
+//     let value = 2;
 
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] == value) {
-            console.log("True")
-        }
-    }
-}
+//     for (let i = 0; i < arr.length; i++){
+//         if (arr[i] == value) {
+//             console.log("True")
+//         }
+//     }
+// }
 
-findValue([1,2,5,6])
+// findValue([1,2,5,6])
+
+var arr = [1, 2, 3, 4, 5, 6, 7]
+console.log(arr.includes(5))
