7feff8740f9b0ebe4648e0c663d71b79b5a0e16e
Anand Suthar <anand880441@gmail.com>
2025-12-12 13:25:55 +0530

    Add new Methods

--- JavaScript/Problem_solving/lvl7/Q66Find Index of a Value.js
+++ JavaScript/Problem_solving/lvl7/Q66Find Index of a Value.js
@@ -1,13 +1,33 @@
-function findIndex(arr, value) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === value) {
-            return i;
-        }
-    }
-    return -1;
-}
+// function findIndex(arr, value) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === value) {
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
 
 
-console.log(findIndex([10, 20, 30], 20));   
-console.log(findIndex([10, 20, 30], 40));
-console.log(findIndex(["a", "b"], "b")); 
+// console.log(findIndex([10, 20, 30], 20));   
+// console.log(findIndex([10, 20, 30], 40));
+// console.log(findIndex(["a", "b"], "b")); 
+
+
+
+// var arr = [1, 2, 3, 4, 5]
+// var value = 1;
+
+// var arr1 = arr.forEach((data, index) => {
+//     if (data === value) { 
+//     console.log(index)
+// }
+// })
+
+
+
+var arr = [1, 2, 3, 4, 5]
+var value = 1;
+
+var index = arr.indexOf(value)
+
+console.log(index)
